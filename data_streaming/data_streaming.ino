7ab9fbcd111d606ada53e42131fabb9b634f08f7
Minju Kang <minju.kang@students.olin.edu>
2017-10-31 12:23:06 -0400

    data streaming arduino code

---
+++ data_streaming/data_streaming.ino
@@ -0,0 +1,57 @@
+// credits to: http://www.meccanismocomplesso.org/en/arduino-tutorial-serial-data-actuator/
+
+
+
+#include <Servo.h>
+
+Servo myservo;
+int angle = 0;
+int newAngle = 0;
+const int MaxChars = 4;
+char strValue[MaxChars+1];
+int index = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  myservo.attach(9);
+  angle = 90;
+}
+
+void loop()
+{}
+
+void serialEvent()
+{
+  while(Serial.available())
+  {
+    char ch = Serial.read();
+    Serial.write(ch);
+    
+    if(index < MaxChars && isDigit(ch)) 
+    {
+      strValue[index++] = ch;
+    }
+    
+    else
+    {
+      strValue[index] = 0;
+      newAngle = atoi(strValue);
+      if (newAngle > 0 && newAngle < 180)
+        if (newAngle < angle)
+          for(; angle > newAngle; angle -= 1) 
+            {
+            myservo.write(angle);
+            }
+        else
+          for(; angle <newAngle; angle += 1)
+          { 
+            myservo.write(angle);
+          }
+    }
+    index = 0;
+    angle = newAngle;
+  }
+}
+
+        
