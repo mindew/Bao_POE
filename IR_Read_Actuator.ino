d89597ec5352b1c4a757a43af269db688d797989
Minju Kang <minju.kang@students.olin.edu>
2017-11-06 18:58:58 -0500

    Add files via upload

---
+++ IR_Read_Actuator.ino
@@ -0,0 +1,21 @@
+#include <Servo.h>
+Servo servo;   // create servo object to control a servo
+
+// Variables
+const int sensorPin = A1;   // select the pin for IR sensor
+int sensor = 0;             // define the class for the sensor value
+float pos;                  // position of the servo
+float ratio;                // ratio of the sensor value and 180 degrees
+// ratio is acquired by: 1023/180
+
+void setup() {
+  Serial.begin(9600);      // start serial monitor
+  servo.attach(9);          // attaches the servo on pin 9
+  }
+
+void loop() {
+  sensor = analogRead(sensorPin);     // read the IR sensor value
+  delay(2);                           // waits 2 ms to settle the reading value
+  pos = sensor / ratio;               // converts the sensor value to position degrees
+  servo.write(pos);                   // tell servo to go to set position
+}
---
+++ pixelate.py
@@ -0,0 +1,132 @@
+# Credits to: https://gist.github.com/scottrogowski/c3c474a04d7c0cd8ddeb
+
+# This script will pixelate most jpg
+# and convert the pixelated image into grayscale
+# It will both show you the result and save it
+# shown image color looks off than what it is supposed to be,
+# maybe because of conflict between PIL library and scipy.
+# however, when you save the image, it works perfectly fine
+# works in python 3
+
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL.Image as Image
+import scipy.misc as misc
+
+
+def load_img(filename):
+    # boilerplate code to open an image and make it editable
+    # opens the file and convert it into grayscale by using PIL mode
+    img = Image.open(filename)
+    data = np.array(img)
+    # convert a grayscale image into array of data
+    return data
+
+
+def all_square_pixels(row, col, square_h, square_w):
+    # Every pixel for a single "square" (superpixel)
+    # Note that different squares might have different dimensions in order to
+    # not have extra pixels at the edge not in a square. Hence: int(round())
+    for y in range(int(round(row*square_h)), int(round((row+1)*square_h))):
+        # renders through row values
+        for x in range(int(round(col*square_w)), int(round((col+1)*square_w))):
+            # renders through column values
+            yield y, x
+            # yield reads only once, number of pixels in original image
+
+
+def make_one_square(img, row, col, square_h, square_w):
+    # Sets all the pixels in img for the square given by (row, col) to that
+    # square's average color
+    pixels = []
+
+    # get all pixels
+    for y, x in all_square_pixels(row, col, square_h, square_w):
+        pixels.append(img[y][x])
+
+    # get the average color
+    av_r = 0
+    av_g = 0
+    av_b = 0
+
+    for r, g, b in pixels:
+        # add rgb values as it goes through for loop
+        av_r += r
+        av_g += g
+        av_b += b
+    av_r /= len(pixels)
+    av_g /= len(pixels)
+    av_b /= len(pixels)
+
+    # set all pixels to that average color
+    for y, x in all_square_pixels(row, col, square_h, square_w):
+        img[y][x] = (av_r, av_g, av_b)
+
+<<<<<<< HEAD
+    # print(av_r, av_g, av_b)
+    avg_Gray = int(round(0.2989 * av_r + 0.5879 * av_g + 0.1140 * av_b))
+    print(avg_Gray)
+=======
+    print(av_r, av_g, av_b)
+>>>>>>> 75f07672cdb33a891d35f026ec2e8db70e174f79
+    # return the average value for rgb in array
+    # a = np.asarray([av_r], [av_g], [av_b])
+
+    # saves the array as .csv file that will used in data streaming
+    # np.savetxt("rgb_values.csv", a, delimiter=",")
+
+
+def rgb2gray(rgb):
+    # converts rgb value into grayscale
+    # grabs the rgb data from an image and converts it into intensity
+    # Y' = 0.2989r + 0.5879g + 0.1140b is used for formula
+    r, g, b = rgb[:, :, 0], rgb[:, :, 1], rgb[:, :, 2]
+    gray = 0.2989 * r + 0.5879 * g + 0.1140 * b
+
+    return gray
+
+
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+        # save user's input on cmd line as filename
+    except IndexError:
+        filename = input("Choose an image to pixelate ")
+        # if there's no input with keyboard, ask what image to pixelate
+    img = load_img(filename)
+    # load the iamge
+
+    # Figure out the dimensions of each square
+    num_cols = int(input("How many squares from left to right? "))
+    # width of the square
+    square_w = float(img.shape[1]) / num_cols
+
+    # determine the row number based on the width of the square
+    num_rows = int(round(img.shape[0] / square_w))
+    # height of the square
+    square_h = float(img.shape[0]) / num_rows
+
+    # overwrite each square with the average color, one by one
+    # basically, part of an image is colored as average color
+    # call function for entire image
+    for row in range(num_rows):
+        for col in range(num_cols):
+            make_one_square(img, row, col, square_h, square_w)
+
+    # convert the image into gray scale
+    # if you want to use rgb image, just comment this line out
+    gray = rgb2gray(img)
+
+    # show the image
+    plt.axis('off')
+    plt.imshow(gray, cmap='gray')
+    plt.show()
+
+    # save the image as filename_pixelated
+    filename_parts = filename.rsplit('.', 1)
+    filename_parts[0] += '_pixelated'
+    filename = '.'.join(filename_parts)
+    print("Saving Pixelated Image")
+
+misc.imsave(filename, gray)
